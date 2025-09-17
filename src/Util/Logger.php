6c648f90642f2da750dda1350f06f3d9e755abd5
Anders <ethananders97@gmail.com>
2025-09-16 21:11:46 -0700

    Sync (alias).

--- src/Util/Logger.php
+++ src/Util/Logger.php
@@ -3,16 +3,13 @@
 namespace Wisp\Util;
 
 use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
 use Wisp\Environment\Runtime;
 
 class Logger extends AbstractLogger
 {
    public function log ($level, string | \Stringable $message, array $context = []) : void
    {
-      if (!container (Runtime::class)->isDebug ()) {
-         return;
-      }
-
       $message = strtr ($message, array_map (
          fn ($v) => @ (string) $v,
          
@@ -22,8 +19,23 @@ class Logger extends AbstractLogger
          )
       ));
 
-      echo "[" . ucfirst ($level) . "] $message";
+      $message = "[" . ucfirst ($level) . "] $message";
       
+      if ($level === LogLevel::NOTICE ||
+          $level === LogLevel::WARNING ||
+          $level === LogLevel::ERROR ||
+          $level === LogLevel::CRITICAL ||
+          $level === LogLevel::ALERT ||
+          $level === LogLevel::EMERGENCY) {
+         error_log ($message . ' ' . json_encode ($context, JSON_PRETTY_PRINT));
+      }
+
+      if (!container (Runtime::class)->isDebug ()) {
+         return;
+      }
+
+      echo $message;
+
       if (!empty ($context)) {
          echo ' ' . json_encode ($context, container (Runtime::class)->isCli () ? 0 : JSON_PRETTY_PRINT);
       }
